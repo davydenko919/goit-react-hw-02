3971baa575bf59592bcdffdb81845c14f64318ce
davydenko919 <davydenko919@gmail.com>
2024-08-01 19:37:48 +0300

    s2

---
+++ src/components/Feedback/Feedback.jsx
@@ -0,0 +1,19 @@
+import css from "./Feedback.module.css";
+
+export default function Feedback() {
+  return (
+    <div>
+
+        <button >Good</button>
+        <button >Neutral</button>
+        <button >Bad</button>
+        <button >Reset</button>
+
+    </div>
+  );
+}
+
+
+{/* <button onClick={}></button> */}
+
+
